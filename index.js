3ddaf8b6baec1ea125f44f752d87c9882fea31d9
Daniel Jimenez <djimenez017@gmail.com>
2022-11-06 22:07:44 -0800

    finished menu

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let date = new Date().getFullYear();
-
+//uses the Date object https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 document.querySelector("#year").innerHTML = date;
+
+const navSliderHandler = () => {
+  const burger = document.querySelector(".hamburger-menu");
+  const nav = document.querySelector(".nav-items");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-items-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSliderHandler();
